44c8f74feaa45c72619c3b566043583d195e2332
Tom Gregor <tgregor@ivsoftware.com>
2022-12-05 10:00:02 -0700

    Add project files.

---
+++ text-to-pic/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace text_to_pic
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.labelMatchCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(216, 47);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 150);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(25, 48);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(163, 31);
+            this.textBoxSearch.TabIndex = 1;
+            // 
+            // labelMatchCount
+            // 
+            this.labelMatchCount.AutoSize = true;
+            this.labelMatchCount.Location = new System.Drawing.Point(223, 51);
+            this.labelMatchCount.Name = "labelMatchCount";
+            this.labelMatchCount.Size = new System.Drawing.Size(93, 25);
+            this.labelMatchCount.TabIndex = 2;
+            this.labelMatchCount.Text = "0 Matches";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.labelMatchCount);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Label labelMatchCount;
+    }
+}
+
---
+++ text-to-pic/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace text_to_pic
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+            // Make an array of the image file names so you can search it.
+            _images = 
+                Directory
+                .GetFiles(folder)
+                .ToArray();
+            textBoxSearch.TextChanged += ontextBoxSearchChanged;
+        }
+        private readonly string[] _images;
+
+        private void ontextBoxSearchChanged(object sender, EventArgs e)
+        {
+            // Do not block on this event.
+            BeginInvoke((MethodInvoker)delegate 
+            {
+                string[] matches;
+                if(string.IsNullOrWhiteSpace(textBoxSearch.Text))
+                {
+                    matches = new string[0];
+                }
+                else
+                {
+                    matches = 
+                    _images
+                    .Where(_ => 
+                        Path.GetFileNameWithoutExtension(_)
+                        .Contains(textBoxSearch.Text)
+                    ).ToArray();
+                }
+                labelMatchCount.Text = $"{matches.Length} matches";
+                if(matches.Length.Equals(1))
+                {
+                    labelMatchCount.Visible = false;
+                    pictureBox.Image = Image.FromFile(matches.First());
+                }
+                else
+                {
+                    pictureBox.Image = null;
+                    labelMatchCount.Visible = true;
+                }
+            });
+        }
+    }
+}
